e58fc844203ed7ebe58197fb2052af4c4563e543
AMess33 <amess3309@gmail.com>
2023-06-14 12:53:33 -0500

    update app.js and add componenets

---
+++ learnreact/src/App.js
@@ -0,0 +1,17 @@
+import Header from "./components/";
+import Content from "./components/";
+import Footer from "./components/";
+
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Content />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
