b4b5a436077407d40613bf8d0b66c449cde5ab8c
AMess33 <amess3309@gmail.com>
2023-07-24 22:06:22 -0500

    change contact to dice

---
+++ learnreact/src/components/Dice.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Dice() {
+  return (
+    <div>
+      <h1> Dice Roll </h1>
+      <div>
+        <br />
+        <br />
+      </div>
+    </div>
+  );
+}
+
+export default Dice;
