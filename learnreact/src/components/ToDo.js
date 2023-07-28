38274adf6037243a5fe55246bab0cdf7d728defb
AMess33 <amess3309@gmail.com>
2023-07-27 19:38:21 -0500

    basic outline for todo component

--- learnreact/src/components/ToDo.js
+++ learnreact/src/components/ToDo.js
@@ -1,7 +1,17 @@
 import React from "react";
 
 function ToDoList() {
-  return <div></div>;
+  return (
+    <div>
+      <div>
+        <h2>My To Do List</h2>
+      </div>
+      <div>
+        <input>New To Do</input>
+        <button>Add to List</button>
+      </div>
+    </div>
+  );
 }
 
 export default ToDoList;
