7435ae27645dd54d3fdf70f8c8f6ea1a6a68fdda
AMess33 <amess3309@gmail.com>
2023-07-27 19:34:09 -0500

    footer text

--- learnreact/src/components/Footer.js
+++ learnreact/src/components/Footer.js
@@ -1,7 +1,12 @@
 import React from "react";
 
 function Footer() {
-  return <div></div>;
+  return (
+    <div>
+      <h4>Copyright Andrew Messer 2023</h4>
+      <h5>GitHub</h5>
+    </div>
+  );
 }
 
 export default Footer;
